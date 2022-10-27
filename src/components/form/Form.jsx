ed477d764db55070d2018c6b7b3d37507f076d40
Onufriieva <onufrieva.julia@gmail.com>
2022-10-27 13:29:56 +0300

    remove getContacts from form

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -2,7 +2,7 @@ import { nanoid } from 'nanoid';
 import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { addContact } from '../../redux/operations';
-import { getContacts, getItems } from '../../redux/selectors';
+import { getItems } from '../../redux/selectors';
 import { FormBox, ButtonAdd, InputBox, LabelBox } from './FormStyled';
 
 
